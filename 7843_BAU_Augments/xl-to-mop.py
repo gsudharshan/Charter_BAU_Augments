871328e7803671ff74f5547a3e4db43d45dbd3dd
gsudharshan <64332536+gsudharshan@users.noreply.github.com>
2020-06-14 10:44:23 -0600

    Update xl-to-mop.py

--- 7843_BAU_Augments/xl-to-mop.py
+++ 7843_BAU_Augments/xl-to-mop.py
@@ -4,6 +4,7 @@ import sys
 filename = sys.argv[1]
 cmd1 = 'python augment.py ' + filename
 cmd2 = 'ansible-playbook -i router_names.yml generate_router_config.yml'
+cmd3 = 'rm generate_router_config.retry'
 os.system(cmd1)
 os.system(cmd2)
-
+os.system(cmd3)
